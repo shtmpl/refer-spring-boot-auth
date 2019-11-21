cf7957e90bc3c599064b4abd6ebecec97ab2054c
ibardych <shtmpbox@gmail.com>
2019-11-21 16:14:45 +0300

    Refactor

--- src/main/java/refer/spring/boot/auth/controller/api/AuthController.java
+++ src/main/java/refer/spring/boot/auth/controller/api/AuthController.java
@@ -1,20 +1,12 @@
 package refer.spring.boot.auth.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import refer.spring.boot.auth.controller.api.request.RequestAuthSignIn;
-import refer.spring.boot.auth.controller.api.request.RequestAuthSignUp;
 import refer.spring.boot.auth.controller.api.response.ResponseAccount;
 import refer.spring.boot.auth.service.AccountService;
 import refer.spring.boot.auth.service.AuthService;
-
-import javax.validation.Valid;
-
 
 @RequestMapping("/api/auth")
 @RestController
@@ -31,27 +23,11 @@ public class AuthController {
     }
 
     @GetMapping("/id")
-    public ResponseEntity<ResponseAccount> identify() {
+    public ResponseAccount identify() {
         return authService.findOwnAccountUsername()
                 .flatMap(accountService::findAccount)
                 .map(ApiMapper.INSTANCE::toResponseAccount)
-                .map(ResponseEntity::ok)
                 .orElseThrow(() ->
                         new UnauthenticatedException("Not authenticated"));
-    }
-
-    @PostMapping("/sign-up")
-    public ResponseEntity<?> signUp(@Valid @RequestBody RequestAuthSignUp request) {
-        return ResponseEntity.ok("Signed up");
-    }
-
-    @PostMapping("/sign-in")
-    public ResponseEntity<?> signIn() {
-        return ResponseEntity.ok("Signed in");
-    }
-
-    @PostMapping("/sign-out")
-    public ResponseEntity<?> signOut() {
-        return ResponseEntity.ok("Signed out");
     }
 }
